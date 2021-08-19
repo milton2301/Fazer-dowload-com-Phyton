f1b6269697d18c002db33ea4362a5429de8c62fa
Amilton Gomes de Lara Filho <79114395+milton2301@users.noreply.github.com>
2021-08-19 20:34:43 -0300

    Fazer dowload com Phyton

---
+++ dowloader.py
@@ -0,0 +1,22 @@
+
+from pytube import YouTube
+
+# Digite o link do vídeo e o local que deseja salvar o video #
+link = input("Insira o link do vídeo que deseja baixar:  ")
+path = input("Caminho da pasta para salvar o vídeo:  ")
+yt = YouTube(link)
+
+# Mostra os detalhes do video #
+print("Título: ", yt.title)
+print("Número de views: ", yt.views)
+print("Tamanho do vídeo: ", yt.length, "segundos")
+print("Avaliação do vídeo: ", yt.rating)
+
+# Usa a maior resolucao #
+ys = yt.streams.get_highest_resolution()
+
+# Começa o Download do vídeo #
+print("Baixando...")
+ys.download(path)
+print("Download completo!")
+input("Aperte qualquer tecla para sair")
